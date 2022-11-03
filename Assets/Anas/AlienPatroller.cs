53424146e36d67e3462edf4bdb0476b53005c997
Anas Bakhsh <bakhsh_anas@yahoo.com>
2022-11-03 12:04:10 +0300

    bugfixes

--- Assets/Anas/AlienPatroller.cs
+++ Assets/Anas/AlienPatroller.cs
@@ -8,25 +8,37 @@ public class AlienPatroller : MonoBehaviour
     [SerializeField] private NavMeshAgent mNavMeshAgent;
     [SerializeField] private Transform[] patrolWaypoints;
     private int currentIndex = 0;
+    private bool invoked = false;
 
     private void Start()
     {
+        //check if unset and try setting it
         if (mNavMeshAgent == null)
         {
             mNavMeshAgent = GetComponentInChildren<NavMeshAgent>();
         }
+        
+        //start patrolling
         Invoke("Patrol", 1f);
     }
 
     private void Update()
     {
+        //if nav has no path calculating
         if (!mNavMeshAgent.pathPending)
         {
+            //if nav reached the stopping distance
             if (mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
             {
+                //if stopped moving or has no path
                 if (!mNavMeshAgent.hasPath || mNavMeshAgent.velocity.sqrMagnitude == 0f)
                 {
-                    Invoke("Patrol", 5f);
+                    //only invoke once
+                    if (!invoked)
+                    {
+                        invoked = true;
+                        Invoke("Patrol", 5f);
+                    }
                 }
             }
         }
@@ -34,6 +46,17 @@ public class AlienPatroller : MonoBehaviour
 
     private void Patrol()
     {
-        //TODO pathing
+        //set new destination from list of waypoints
+        mNavMeshAgent.SetDestination(patrolWaypoints[currentIndex].position);
+
+        //move index for next destination in list
+        currentIndex++;
+        
+        //loop list of waypoints
+        if (currentIndex == patrolWaypoints.Length)
+            currentIndex = 0;
+        
+        //reset bool of invoke for next activation
+        invoked = false;
     }
 }
